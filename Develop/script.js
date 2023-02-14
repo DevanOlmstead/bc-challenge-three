a01551287570f5c8f74b5b77ac6f2f4558584b83
Devan Olmstead <devanolmstead@gmail.com>
2023-02-14 12:07:34 -0800

    second commit

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,15 @@
-// Assignment code here
 
+var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
+writePassword = window.alert("Your password is " + generate);
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = "doodookahkah";
   var passwordText = document.querySelector("#password");
-
+  console.log(password);
   passwordText.value = password;
 
 }
